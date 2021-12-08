814814c9c31a19a1f7400f88b93fb892782ac28c
kisb01 <BudaPest12>
2021-12-08 17:49:33 +0100

    Add Project Lombok and Refactor. Closes #51.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -1,12 +1,29 @@
 package guru.springframework.petclinic.model;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
+
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 
     @Column(name = "address")
     private String address;
@@ -18,37 +35,19 @@ public class Owner extends Person{
     private String telephone;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    @ToString.Exclude
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Owner owner = (Owner) o;
+        return getId() != null && Objects.equals(getId(), owner.getId());
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
     }
 }
